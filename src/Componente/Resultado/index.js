9f13f20ba4714043fd20ced5e92cc73fde00acab
Bruno Aoyagi <bruno.sanches@clevertech.biz>
2024-02-15 15:52:17 -0300

    Corrige a passagem do resultado

--- src/Componente/Resultado/index.js
+++ src/Componente/Resultado/index.js
@@ -1,24 +1,29 @@
-  import React from 'react';
-  import './Resultado.css';
+import React from "react";
+import "./Resultado.css";
 
+/* como seria a tipagem no typescript:
+ *
+ * type Props = {
+ *  calculoGeral: {
+ *    resultado: number
+ *  }
+ *  resultadoMedia: number
+ * }
+ */
 
-  const Resultado = (props) => {
+const Resultado /* : FunctionComponent<Props> */ = (props) => {
+  return (
+    <section className="resultado">
+      <form className="resultado">
+        <h3 className="resultado-box">
+          <label>Resultado da :{props.calculoGeral.resultado} </label>
+          <br />
+          Resultado da Média : {props.resultadoMedia}
+          <br />O resultado
+        </h3>
+      </form>
+    </section>
+  );
+};
 
-    return (
-      <section className='resultado'>
-        <form className='resultado'>
-          <h3 className='resultado-box'>
-            <label>Resultado da :{props.calculoGeral.resultado} </label>
-            <br />
-            Resultado da Média : {props.resultadoMedia}
-            <br />
-            O resultado 
-          </h3>
-        </form>
-      </section>
-    );
-  };
-
-  export default Resultado;
-
-  
+export default Resultado;
